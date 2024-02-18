949386caeb778a0cae4add7bfd36240421b7f767
Ayushhhh <badassbrahmin77@gmail.com>
2024-02-18 17:39:33 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/skycast/Main.kt
@@ -0,0 +1,12 @@
+package com.example.skycast
+
+data class Main(
+    val feels_like: Double,
+    val grnd_level: Int,
+    val humidity: Int,
+    val pressure: Int,
+    val sea_level: Int,
+    val temp: Float,
+    val temp_max: Float,
+    val temp_min: Float
+)
---
+++ app/src/main/java/com/example/skycast/Wind.kt
@@ -0,0 +1,7 @@
+package com.example.skycast
+
+data class Wind(
+    val deg: Int,
+    val gust: Double,
+    val speed: Double
+)
